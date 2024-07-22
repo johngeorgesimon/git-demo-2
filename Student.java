950889628ff9cfdd7a24b698de36b64c91a410c1
johngeorgesimon <johng4101@gmail.com>
2024-07-22 11:02:13 +0530

    added args const

--- Student.java
+++ Student.java
@@ -7,6 +7,11 @@ class Student{
 
     }
 
+    public Student(int id, String name, int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
     public int getId() {
         return id;
     }
